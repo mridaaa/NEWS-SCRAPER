9bda1cf6dc2340cc3492694aad8d5b73dc0312f4
Mrida <68474511+mridaaa@users.noreply.github.com>
2023-12-28 15:03:53 -0500

    Update NewsScraper.java

--- NewsScraper.java
+++ NewsScraper.java
@@ -1,3 +1,11 @@
+/*
+ * December 28, 2023
+ * Author: Mrida Yawale
+ *
+ */
+
+import java.util.Scanner;
+
 public class NewsScraper {
     public String getHTML(String url) {
         URLReader ur = new URLReader(url);
@@ -5,15 +13,19 @@ public class NewsScraper {
         return html;
     }
 
-    public String getHeadlines(String html, String pattern) {
+    public News getHeadlines(String html, String pattern) {
         int beginIndex = html.indexOf(pattern);
 
-        // Default to 0, Assign in the while loop
+        // Default to 0, assigned in the while loop
         int endIndex = 0;
 
         String newsItemHTML = " ";
 
+        News news = new News();
+        int count = 1;
+
         while (beginIndex >= 0 && endIndex >= 0) {
+
             // end index for the current look up is the begin index for the next lookup
             // Next look up
             endIndex = html.indexOf(pattern, beginIndex + 1);
@@ -23,18 +35,35 @@ public class NewsScraper {
 
             newsItemHTML = html.substring(beginIndex, endIndex);
             NewsItem newsItem = new NewsItem(newsItemHTML);
-            System.out.println(newsItem.getSummary());
+
+            if (count == 1) {
+                news.setFirstItem(newsItem);
+            } else if (count == 2) {
+                news.setSecondItem(newsItem);
+            } else if (count == 3) {
+                news.setThirdItem(newsItem);
+            }
+            count ++;
+
             // Begin index for next look up is the end index of the last look up
             beginIndex = endIndex;
 
         }
-        return null;
+        return news;
     }
 
     public static void main(String[] args) {
+
+        Scanner ear = new Scanner(System.in);
+        System.out.println("Enter a topic: ");
+        String newsTopic = ear.nextLine();
+
         NewsScraper ns = new NewsScraper();
-        String html = ns.getHTML("https://www.bing.com/news/search?q=2024+election");
-        ns.getHeadlines(html, "<div class=\"news-card newsitem cardcommon\"");
+        String url = Constants.NEWS_SITE_URL + newsTopic.replace(" ", "+");
+        String html = ns.getHTML(url);
+
+        News news = ns.getHeadlines(html, "<div class=\"news-card newsitem cardcommon\"");
+        System.out.println(news);
     }
 
     public String getFirst(String headlines) {
