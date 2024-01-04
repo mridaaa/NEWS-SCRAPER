98e509bf7f034708d64f0f7bd4116e19fad130b3
Mrida <68474511+mridaaa@users.noreply.github.com>
2024-01-04 08:19:26 -0500

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,14 @@
+public class Constants {
+    public static final String NEWS_SITE_URL = "https://www.bing.com/news/search?count=50&offset=0&q=";
+    public static final String NOT_AVAILABLE = "**NOT AVAILABLE**";
+    public static final String NEWS_ITEM_PATTERN = "<div class=\"news-card newsitem cardcommon\"";
+    public static final String LOGO_START_PATTERN = "src=\"/th?id=";
+    public static final String LOGO_END_PATTERN = "&qlt=30\"";
+    public static final String POST_TIME_START_PATTERN = "ago\">";
+    public static final String POST_TIME_END_PATTERN = "ago\">";
+    public static final String HEADLINE_START_PATTERN = "data-title=\"";
+    public static final String HEADLINE_END_PATTERN = "data-author=";
+    public static final String SUMMARY_START_PATTERN = "...\">";
+    public static final String SUMMARY_END_PATTERN = "...</div>";
+
+}
